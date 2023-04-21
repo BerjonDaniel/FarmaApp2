599b17086c5c6f47c53007688665240993703863
Daniel Berjon <danielberjon@solvium.es>
2023-04-21 12:49:14 +0200

    ContextTest añadido

--- app/src/androidTest/java/com/example/farmaapp2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/farmaapp2/ExampleInstrumentedTest.java
@@ -1,8 +1,7 @@
 package com.example.farmaapp2;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +13,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+/*
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
@@ -23,3 +23,5 @@ public class ExampleInstrumentedTest {
         assertEquals("com.example.farmaapp2", appContext.getPackageName());
     }
 }
+
+ */
